ee3da2420be2e9559fa9015e0015d4c2858879ac
adrian-jaimes2430 <75454823+adrian-jaimes2430@users.noreply.github.com>
2026-08-30 22:26:42 -0500

    feat: route immersive history page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
+import History from "./pages/History.tsx";
 import NotFound from "./pages/NotFound.tsx";
 import Inverfact from "./pages/Inverfact.tsx";
 import Anma from "./pages/Anma.tsx";
@@ -20,11 +21,11 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/history" element={<History />} />
           <Route path="/inverfact" element={<Inverfact />} />
           <Route path="/anma" element={<Anma />} />
           <Route path="/nomadhive" element={<Nomadhive />} />
           <Route path="/unsubscribe" element={<Unsubscribe />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
